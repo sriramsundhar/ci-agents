628fc19ef76b9759449b31e661e67d0aabc9df8d
Sriram Sundharesan <sriram.sundhar@gmail.com>
2025-10-14 00:48:29 -0400

    Add platform support (#8)
    * Add platform support
    
    * Add platform support
    
    * update docker plugin
    
    * Add platform support
    
    * Add platform support
    
    * Add platform support
    
    * Add platform support
    
    * Add platform support
    
    * Add platform support
    
    * Add platform support
    
    * Add platform support
    
    * Add platform support
    
    * Add platform support
    
    * Add platform support
    
    * Add platform support
    
    * Add platform support
    
    * Add platform support
    
    ---------
    
    Co-authored-by: name <email@smtp.com>

--- mongo/build.gradle.kts
+++ mongo/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.palantir.docker") version "0.30.0"
+    id("com.palantir.docker") version "0.36.0"
 }
 
 tasks.create("publish") {
@@ -8,8 +8,10 @@ tasks.create("publish") {
 
 docker {
     name = "sriramsundhar/${project.name}"
-    tag("latest", "sriramsundhar/${project.name}:latest")
-    tag("${project.version}", "sriramsundhar/${project.name}:${project.version}")
+    tag("latest", "${project.version}")
+    //tag("${project.version}", "sriramsundhar/${project.name}:${project.version}")
+    buildx(true)
+    platform("linux/amd64","linux/arm64")
     copySpec.from("sync").into("sync")
 
 }
