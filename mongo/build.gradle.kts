de880d91c4e1116243b170c183944bd179183e60
name <email@smtp.com>
2025-10-14 13:41:33 -0400

    move to 36

--- mongo/build.gradle.kts
+++ mongo/build.gradle.kts
@@ -1,10 +1,10 @@
-plugins { id("com.palantir.docker") version "0.30.0" }
+plugins { id("com.palantir.docker") version "0.36.0" }
 
 tasks.create("publish") { dependsOn(getTasksByName("dockerTagsPush", true)) }
 
 docker {
   name = "sriramsundhar/${project.name}"
   tag("latest", "sriramsundhar/${project.name}:latest")
-  tag("${project.version}", "sriramsundhar/${project.name}:${project.version}")
+  tag("${project.name}:${project.version}", "sriramsundhar/${project.name}:${project.version}")
   copySpec.from("sync").into("sync")
 }
