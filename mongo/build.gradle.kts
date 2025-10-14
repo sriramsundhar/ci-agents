aec3de04c01a2e95d3f00f46dfd4484fe26527c9
name <email@smtp.com>
2025-10-14 13:09:20 -0400

    try

--- mongo/build.gradle.kts
+++ mongo/build.gradle.kts
@@ -1,17 +1,13 @@
-plugins {
-    id("com.palantir.docker") version "0.36.0"
-}
+plugins { id("com.palantir.docker") version "0.36.0" }
 
-tasks.create("publish") {
-    dependsOn(getTasksByName("dockerTagsPush", true))
-}
+tasks.create("publish") { dependsOn(getTasksByName("dockerTagsPush", true)) }
 
 docker {
-    name = "sriramsundhar/${project.name}"
-    tag("dockerhub", "sriramsundhar/${project.name}:latest")
-    tag("dockerhub2", "sriramsundhar/${project.name}:${project.version}")
-    buildx(true)
-    platform("linux/amd64","linux/arm64")
-    copySpec.from("sync").into("sync")
-
+  name = "sriramsundhar/${project.name}"
+  tag("dockerhub", "sriramsundhar/${project.name}:latest")
+  tag("dockerhub2", "sriramsundhar/${project.name}:${project.version}")
+  buildx(true)
+  platform("linux/amd64", "linux/arm64")
+  load(true)
+  copySpec.from("sync").into("sync")
 }
